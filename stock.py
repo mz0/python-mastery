9aae79c80ca54365ef561ca0086e68b91a0e2d56
Mark Zhitomirski <marcuzero@gmail.com>
2025-10-20 18:51:19 +0400

    exercise 3.1

--- stock.py
+++ stock.py
@@ -3,5 +3,24 @@ class Stock:
         self.name = name
         self.shares = shares
         self.price = price
+
     def cost(self):
         return self.shares * self.price
+
+    def sell(self, qty):
+        self.shares -= qty
+
+
+def read_portfolio(filename):
+    portf = list()
+    with open(filename) as f:
+        f.readline()  # skip headers
+        for line in f:
+            r = line.split(',')
+            portf.append(Stock(r[0].strip('"'), int(r[1]), float(r[2])))
+    return portf
+
+if __name__ == '__main__':
+    portfolio = read_portfolio('Data/portfolio.csv')
+    for s in portfolio:
+        print('%10s %10d %10.2f' % (s.name, s.shares, s.price))
