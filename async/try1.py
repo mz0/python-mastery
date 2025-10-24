36544cf1ae3babeab460eb9ed877d82f1729e313
Mark Zhitomirski <marcuzero@gmail.com>
2025-10-24 20:00:57 +0400

    realpython.com/python-async-features 3/6

--- async/try1.py
+++ async/try1.py
@@ -2,37 +2,43 @@
 # https://realpython.com/python-async-features/
 
 import queue
+import time
+from codetiming import Timer
+
 
 def task(name, work_queue):
-    while not work_queue.empty():
-        count = work_queue.get()
-        total = 0
-        print(f"Task {name} running")
-        for x in range(count):
-            total += 1
-            yield
-        print(f"Task {name} total: {total}")
+  timer = Timer(text=f"Task {name} elapsed time: {{:.1f}}")
+  while not work_queue.empty():
+    delay = work_queue.get()
+    print(f"Task {name} running")
+    timer.start()
+    time.sleep(delay/10)
+    timer.stop()
+    yield
+
 
 def main():
-    work_queue = queue.Queue()  # Create the queue of work
+  work_queue = queue.Queue()  # Create the queue of work
 
-    # Put some work in the queue
-    for work in [15, 10, 5, 2]:
-        work_queue.put(work)
+  # Put some work in the queue
+  for work in [15, 10, 5, 2]:
+    work_queue.put(work)
 
-    # Create some tasks
-    tasks = [task("One", work_queue), task("Two", work_queue)]
+  # Create some tasks
+  tasks = [task("One", work_queue), task("Two", work_queue)]
 
-    # Run the tasks
-    done = False
+  # Run the tasks
+  done = False
+  with Timer(text="\nTotal elapsed time: {:.1f}"):
     while not done:
-        for t in tasks:
-            try:
-                next(t)
-            except StopIteration:
-                tasks.remove(t)
-            if len(tasks) == 0:
-                done = True
+      for t in tasks:
+        try:
+          next(t)
+        except StopIteration:
+          tasks.remove(t)
+        if len(tasks) == 0:
+          done = True
+
 
 if __name__ == "__main__":
-    main()
+  main()
