a60dde9f21682fc87751b0aecf85fe3abee2cc4c
Mark Zhitomirski <marcuzero@gmail.com>
2025-10-24 19:45:38 +0400

    realpython.com/python-async-features 2/6

--- async/try1.py
+++ async/try1.py
@@ -4,16 +4,14 @@
 import queue
 
 def task(name, work_queue):
-    if work_queue.empty():
-        print(f"Task {name} nothing to do")
-    else:
-        while not work_queue.empty():
-            count = work_queue.get()
-            total = 0
-            print(f"Task {name} running")
-            for x in range(count):
-                total += 1
-            print(f"Task {name} total: {total}")
+    while not work_queue.empty():
+        count = work_queue.get()
+        total = 0
+        print(f"Task {name} running")
+        for x in range(count):
+            total += 1
+            yield
+        print(f"Task {name} total: {total}")
 
 def main():
     work_queue = queue.Queue()  # Create the queue of work
@@ -22,12 +20,19 @@ def main():
     for work in [15, 10, 5, 2]:
         work_queue.put(work)
 
-    # Create some synchronous tasks
-    tasks = [(task, "One", work_queue), (task, "Two", work_queue)]
+    # Create some tasks
+    tasks = [task("One", work_queue), task("Two", work_queue)]
 
     # Run the tasks
-    for t, n, q in tasks:
-        t(n, q)
+    done = False
+    while not done:
+        for t in tasks:
+            try:
+                next(t)
+            except StopIteration:
+                tasks.remove(t)
+            if len(tasks) == 0:
+                done = True
 
 if __name__ == "__main__":
     main()
